4046307c7cd0ea5741ac7cb9aebc8071c5fb9425
jockehewh <xavier.belenus@gmail.com>
2018-07-24 14:49:27 +0200

    i now save the created pages in a JSON format

--- xcms.js
+++ xcms.js
@@ -1,15 +1,32 @@
 const koa = require('koa')
 const WS = require('ws')
 const fs = require('fs')
-const mongoClient = require('mongodb').MongoClient
+const xcmsDB = require('./xdata.js');
 const xcmsWs = new WS.Server({port: 9898});
 const xcms = new koa();
 const jsp = JSON.parse;
 const jss = JSON.stringify;
+var pagesCollection;
+var dbpages = xcmsDB.get()
+dbpages.on('data', (data)=>{
+    if(pagesCollection === undefined){
+        pagesCollection = data
+    }else{
+        pagesCollection += data
+    }
+})
+dbpages.on('end', ()=>{
+console.log(pagesCollection,'+')
+pagesCollection = JSON.parse(pagesCollection)
+console.log(pagesCollection)
+})
+
 
 xcms.use(async (ctx, next) =>{
+    var urlctl = /([a-z]{2,}).(html|css|js|jpeg)/.exec(ctx.url)
     ctx.type= "html"
-    ctx.body = ctx.url;
+    //ctx.body = ctx.url;
+    console.log(ctx.url)
     switch (ctx.url){
         case '/' :
             ctx.redirect('/admin')
@@ -21,15 +38,38 @@ xcms.use(async (ctx, next) =>{
             ctx.body = fs.createReadStream('./admin-site/index.html',{autoClose: true})
         break;
         case /\/client-site/.test(ctx.url):
-        ctx.body = fs.createReadStream('./'+ctx.url, {autoClose: true})
+        // récuperer le contenu depuis la db  avec ++++  /([a-z]{2,}).(html|css|js|jpeg)/
+        
+        //ctx.body = fs.createReadStream('./'+ctx.url, {autoClose: true})
+        break;
+        default:
+        pagesCollection.forEach(page=>{
+            if(Object.values(page)[0] === urlctl[0]){
+                ctx.body = Object.values(page)[1]
+            }
+        })
         break;
     }
     if(/\.(html|css|js|jpeg)/.test(ctx.url)){
-        console.log(ctx.origin)
+        console.log(/([a-z]{2,}).(html|css|js|jpeg)/.exec(ctx.url)[1])
+        /* console.log(ctx.origin)
         console.log(ctx.href)
-        console.log(ctx.originalUrl)
+        console.log(ctx.originalUrl) */
+        console.log(urlctl)
         ctx.type = /(html|css|js|jpeg)/.exec(ctx.url)[0]
-        ctx.body = fs.createReadStream('./'+ctx.url, {autoClose: true})
+        if(/\/client-site/.test(ctx.url)){
+            //ctx.type = /(html|css|js|jpeg)/.exec(ctx.url)[0]
+            pagesCollection.forEach(page=>{
+                if(Object.values(page)[0] === urlctl[0]){
+                    ctx.body = Object.values(page)[1]
+                }
+            })
+        }else{
+            //ctx.type = urlctl[2]
+            ctx.body = fs.createReadStream('./'+ctx.url, {autoClose: true})
+        }
+        
+        //ctx.body = fs.createReadStream('./'+ctx.url, {autoClose: true})
     }
     await next();
 })
@@ -43,7 +83,6 @@ xcms.listen(9899,()=>{
         this.isAlive = true;
     }
     xcmsWs.on('connection', (peer)=>{
-        console.log(peer)
         peer.isAlive = true;
         peer.on('pong', heartbeat)
         fs.readdir('./client-site', (err, fil)=>{
@@ -80,9 +119,30 @@ xcms.listen(9899,()=>{
                     image = datainfo
                 }
                 if(datainfo.titre){
-                    var newFile = fs.createWriteStream('./client-site/'+datainfo.titre+".html", {encoding: 'utf8'})
+                    nouvellePage = {name: datainfo.titre+".html", page: datainfo.contenu}
+                    count = 0
+                    pagesCollection.forEach(page=>{
+                        if(Object.values(page)[0] === nouvellePage.name){
+                            console.log('erreur la page exist')
+                            count++
+                            return
+                        }
+                    }, /* second callback*/
+                    ()=>{
+                        if(count < pagesCollection.length){
+                            xcmsDB.set(nouvellePage)
+                        }else{
+                            console.log('erreur la page exist pour de vrai')
+                            count = 0
+                            return
+                        }
+                    }
+                )
+                    pagesCollection.push(nouvellePage)
+                    //xcmsDB.set(nouvellePage)
+                    /* var newFile = fs.createWriteStream('./client-site/'+datainfo.titre+".html", {encoding: 'utf8'})
                     newFile.write(datainfo.contenu)
-                    newFile.end()
+                    newFile.end() */
                 }
             }
             if(typeof data === 'object'){
