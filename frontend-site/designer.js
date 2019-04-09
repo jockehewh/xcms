54c08d106db2aeb59804d7bc5cd04e4c2d906139
jockehewh <xavier.belenus@gmail.com>
2019-04-09 10:52:51 +0200

    added smart carrousel
    changed the menu builder to allow link to another page

--- frontend-site/designer.js
+++ frontend-site/designer.js
@@ -117,6 +117,139 @@ window.onload = ()=>{
         document.querySelectorAll('.xcms-div').forEach(div=>{
             div.className += " washed-red bg-blue"
         })
-
+    }
+    if(document.querySelector('.xcms-img') !== null){
+        let imgArray = []
+        let tempArray = document.querySelectorAll('.xcms-img')
+        for (let i = 0; i < tempArray.length+1; i++){
+            if(i !== tempArray.length){
+                if(tempArray[i].nextSibling){
+                    if(tempArray[i].nextSibling.nodeName === 'IMG' ){
+                        imgArray.push({file: tempArray[i].src, index: i, node:tempArray[i]})
+                        continue
+                    }
+                }
+                if(tempArray[i].previousSibling){
+                    if(tempArray[i].previousSibling.nodeName === "IMG"){
+                        imgArray.push({file: tempArray[i].src, index: i, node:tempArray[i]})
+                        continue
+                    }
+                }
+                
+            }
+        }
+        if(imgArray.length >= 3){
+            imgArray.forEach(data=>{
+                data.node.remove()
+            })
+            let visibleImages = {
+                center: 0,
+                left: 0,
+                right: 0
+            }
+            let divCarroussel = document.createElement('div')
+            let leftBtn = document.createElement('button')
+            let rightBtn = document.createElement('button')
+            divCarroussel.style.display = "grid"
+            divCarroussel.style.gridTemplateColumns = "1fr 2fr 4fr 2fr 1fr"
+            divCarroussel.style.gridTemplateRows = "1fr"
+            divCarroussel.style.alignItems = "center"
+            divCarroussel.style.justifyItems = "center"
+            divCarroussel.style.height = '500px'
+            divCarroussel.style.maxHeight = '500px'
+            let currentImage = document.createElement('img')
+            currentImage.style.gridColumn = 3
+            currentImage.style.gridRow = 1
+            currentImage.style.maxHeight = '500px'
+            let leftImage = document.createElement('img')
+            leftImage.style.gridColumn = 2
+            leftImage.style.gridRow = 1
+            leftImage.style.maxHeight = '500px'
+            let rightImage = document.createElement('img')
+            rightImage.style.gridColumn = 4
+            rightImage.style.gridRow = 1
+            rightImage.style.maxHeight = '500px'
+            currentImage.src = imgArray[0].file
+            leftImage.src = imgArray[imgArray.length-1].file
+            rightImage.src = imgArray[1].file
+            visibleImages.left = imgArray[imgArray.length-1].index
+            visibleImages.right = imgArray[1].index
+            leftBtn.innerText = "previous"
+            leftBtn.id = "previous"
+            leftBtn.dataset.demande = "previous"
+            leftBtn.style.gridColumn = 1
+            leftBtn.style.gridRow = 1
+            rightBtn.innerText = "next"
+            rightBtn.id = "next"
+            rightBtn.dataset.demande = "next"
+            rightBtn.style.gridColumn = 5
+            rightBtn.style.gridRow = 1
+            divCarroussel.appendChild(leftBtn)
+            divCarroussel.appendChild(rightBtn)
+            divCarroussel.appendChild(currentImage)
+            divCarroussel.appendChild(leftImage)
+            divCarroussel.appendChild(rightImage)
+            document.body.appendChild(divCarroussel)
+            let btnArray = [rightBtn, leftBtn]
+            btnArray.forEach(btn =>{
+                btn.addEventListener('click', (e)=>{
+                    e.stopPropagation()
+                    switch(e.target.dataset.demande){
+                        case "previous":
+                                visibleImages.center--
+                                visibleImages.right--
+                                visibleImages.left--
+                            if(visibleImages.center === 0 || visibleImages.center < 0){
+                                if(visibleImages.center < 0 ){
+                                    visibleImages.center = imgArray.length - 1
+                                    visibleImages.right = 0
+                                    visibleImages.left = imgArray.length - 2
+                                }else{
+                                    visibleImages.center = 0
+                                    visibleImages.right = 1
+                                    visibleImages.left = imgArray.length - 1
+                                }
+                                currentImage.src = imgArray[visibleImages.center].file
+                                leftImage.src = imgArray[visibleImages.left].file
+                                rightImage.src = imgArray[visibleImages.right].file
+                            }else{
+                                if(visibleImages.left < 0){
+                                    visibleImages.left = imgArray.length -1
+                                }
+                                if(visibleImages.right < 0){
+                                    visibleImages.right = imgArray.length -1
+                                }
+                                currentImage.src = imgArray[visibleImages.center].file
+                                leftImage.src = imgArray[visibleImages.left].file
+                                rightImage.src = imgArray[visibleImages.right].file
+                            }
+                        break;
+                        case "next":
+                                visibleImages.center++
+                                visibleImages.right++
+                                visibleImages.left++
+                            if(visibleImages.center === 0 || visibleImages.center === imgArray.length){
+                                visibleImages.center = 0
+                                visibleImages.right = 1
+                                visibleImages.left = imgArray.length-1
+                                currentImage.src = imgArray[visibleImages.center].file
+                                leftImage.src = imgArray[visibleImages.left].file
+                                rightImage.src = imgArray[visibleImages.right].file
+                            }else{
+                                if(visibleImages.right > imgArray.length-1){
+                                    visibleImages.right = 0
+                                }
+                                if(visibleImages.left === imgArray.length){
+                                    visibleImages.left = 0
+                                }
+                                currentImage.src = imgArray[visibleImages.center].file
+                                leftImage.src = imgArray[visibleImages.left].file
+                                rightImage.src = imgArray[visibleImages.right].file
+                            }
+                        break; 
+                    }
+                })
+            })
+        }
     }
 }
